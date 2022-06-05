38ef68886ac4eef145f368604fd356bd90ea84fa
salkhan-ops <65277891+salkhan-ops@users.noreply.github.com>
2022-06-05 12:05:52 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(ExampleApp());
+main() => runApp(const ExampleApp());
+//
+// class ButtonOverlayColor implements MaterialStateProperty<Color?> {
+//   @override
+//   Color? resolve(Set<MaterialState> states) {
+//     if (states.contains(MaterialState.hovered)) {
+//       return Colors.greenAccent;
+//     } else if (states.contains(MaterialState.pressed)) {
+//       return Colors.lightGreenAccent;
+//     }
+//     return null;
+//   }
+// }
+//
+// class ButtonTextStyle implements MaterialStateProperty<TextStyle?> {
+//   @override
+//   TextStyle? resolve(Set<MaterialState> states) {
+//     return states.contains(MaterialState.pressed)
+//         ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 50)
+//         : null;
+//   }
+// }
+
+// class MaterialStateColor extends Color
+//     implements MaterialStateProperty<Color?> {
+//   MaterialStateColor(super.value);
+//
+//   @override
+//   Color? resolve(Set<MaterialState> states) {
+//     return states.contains(MaterialState.pressed) ? Colors.blue : Colors.pink;
+//   }
+// }
 
 class ExampleApp extends StatelessWidget {
   const ExampleApp({Key? key}) : super(key: key);
@@ -8,23 +39,60 @@ class ExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        textTheme: Typography().black.apply(
-          displayColor: Colors.greenAccent,
-          bodyColor: Colors.green,
-        ).copyWith(displayLarge: const TextStyle(
-          color: Colors.lightGreen,
-          fontSize: 35,
-          fontWeight: FontWeight.w300,
-          letterSpacing: 0.8
-        )
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          inputDecorationTheme: InputDecorationTheme(
+            border: MaterialStateOutlineInputBorder.resolveWith((states) {
+              final isFocused = states.contains(MaterialState.focused);
+              final isDisabled = states.contains(MaterialState.disabled);
+              final hasError = states.contains(MaterialState.error);
+              final color = isDisabled
+                  ? Colors.grey
+                  : hasError
+                      ? Colors.red
+                      : Colors.yellow;
+              final width = isFocused ? 2.0 : 1.0;
+              return OutlineInputBorder(
+                borderSide: BorderSide(color: color, width: width),
+              );
+            }),
 
-        ),
-        brightness: Brightness.dark,
-        iconTheme:
-            const IconThemeData(size: 50, color: Colors.lime, opacity: 0.5),
-      ),
+            // errorStyle: TextStyle(fontStyle: FontStyle.italic),
+            // floatingLabelStyle: TextStyle(
+            //     fontWeight: FontWeight.bold, color: Colors.lightGreen),
+            // hintStyle: TextStyle(
+            //   fontStyle: FontStyle.italic,
+            //   fontSize: 14,
+            // ),
+
+            // enabledBorder: OutlineInputBorder(
+            //   borderSide: BorderSide(color: Colors.lightgreen,),),
+            // focusedBorder: OutlineInputBorder(
+            //     borderSide: BorderSide(color: Colors.green, width: 2)),
+            // errorBorder: OutlineInputBorder(
+            //     borderSide: BorderSide(color: Colors.red)),
+            // focusedErrorBorder: OutlineInputBorder(
+            //     borderSide: BorderSide(color: Colors.redAccent, width: 2)),
+            // border: OutlineInputBorder(
+            //   borderSide: BorderSide(color: Colors.grey),),
+            //suffixIconColor: Colors.red
+          ),
+          textSelectionTheme: const TextSelectionThemeData(
+            cursorColor: Colors.blue,
+            selectionColor: Colors.lime,
+            selectionHandleColor: Colors.brown,
+          ),
+          scaffoldBackgroundColor: Colors.grey,
+          appBarTheme: AppBarTheme(
+            backgroundColor: MaterialStateColor.resolveWith((states) {
+              return states.contains(MaterialState.scrolledUnder)
+                  ? Colors.lime
+                  : Colors.blue;
+            }),
+            elevation: 20.0,
+            actionsIconTheme: IconThemeData(color: Colors.white),
+            centerTitle: false,
+          )),
       debugShowCheckedModeBanner: false,
       home: const ExamplePage(),
     );
@@ -36,23 +104,40 @@ class ExamplePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Consistent Design with Flutter Theme Widget'),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.account_circle)),
-        ],
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(20),
-        child: Center(
-          child: ExampleWidget(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Consistent Design with Flutter Theme Widget'),
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.account_circle)),
+          ],
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: ExampleWidget(),
+          ),
         ),
       ),
     );
   }
 }
-
+// ThemeData(
+// textTheme: Typography().black
+//     .apply(
+// displayColor: Colors.greenAccent,
+// bodyColor: Colors.green,
+// )
+// .copyWith(
+// displayLarge: TextStyle(
+// color: Colors.lightGreen,
+// fontSize: 18.0,
+// fontWeight: FontWeight.bold,
+// letterSpacing: 0.8,
+// ),
+// ),
+// )
 class ExampleWidget extends StatelessWidget {
   const ExampleWidget({Key? key}) : super(key: key);
 
@@ -60,21 +145,28 @@ class ExampleWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text(
-          'displayLarge',
-          style: Theme.of(context).textTheme.displayLarge,
+      children: const [
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'enabled',
+            labelText: 'email',
+            suffixIcon: Icon(Icons.email),
+          ),
         ),
-        Text(
-          'explicit bodyMedium',
-          style: Theme.of(context).textTheme.bodyMedium,
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'enabled error',
+            errorText: 'error',
+            suffixIcon: Icon(Icons.error),
+          ),
         ),
-        const Text(
-          'implicit bodyMedium',
-        ),
-        Text(
-          'labelSmall',
-          style: Theme.of(context).textTheme.labelSmall,
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'disabled',
+            labelText: 'disabled',
+            suffixIcon: Icon(Icons.disabled_by_default),
+          ),
+          enabled: false,
         ),
       ],
     );
